fa01dec6de8b373d37f06df3544dc729c77811d2
Dennis <58036609+dennight@users.noreply.github.com>
2020-12-24 10:53:30 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+// Isotope filter
+$(document).ready(function () {     
+  $('.grid').isotope(function () {
+    itemSelector: '.element-item'
+  });
+  $('.button').click(function () {
+    $('.button').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+    $('.grid').isotope({
+      filter: selector
+    })
+    return false;
+  });
+})
+
+'use strict';
+// Active menu item on scroll
+const links = document.querySelectorAll('.menu-item');
+const sections = document.querySelectorAll('section');
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while (--index && window.scrollY + 50 < sections[index].offsetTop) { }
+
+  links.forEach((link) => link.classList.remove('active'));
+  links[index].classList.add('active');
+}
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
+
+
+// Change background menu on scroll
+var header = document.querySelector('header')
+
+window.onscroll = function () {
+
+  if (window.pageYOffset > 0) {
+    header.classList.add('scrolling')
+  } else {
+    header.classList.remove('scrolling')
+  }
+};
+
+
+// Hamburger
+var hamburger = document.querySelector(".hamburger");
+var navbar = document.getElementById("nav");  
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+  navbar.classList.toggle("is-open");
+  header.classList.toggle("show");
+});
+
